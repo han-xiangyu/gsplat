3f834403e926d496ad94de848ac5e773db1fa6ca
Yiming Li <ymingli@cs-oci-ord-login-01.cm.cluster>
2024-08-10 09:18:05 -0700

    update script

---
+++ run_train_noDensify.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# LOC=loc0_4
+# LOC=loc1975_1
+# LOC=loc1200_2
+# LOC=loc1200_1
+LOC=loc2450_1
+
+GPU_NUM=8
+CAP_MAX=500000
+NOISE_SCALE=500000
+OPACITY_REG=0.001
+SCALE_REG=0.001
+DENSIFY_INTER=100
+
+torchrun --standalone --nnodes=1 --nproc_per_node ${GPU_NUM} train.py --bsz ${GPU_NUM} -s /lustre/fs3/portfolios/nvr/users/ymingli/datasets/Ithaca-full/rectified/$LOC -m /lustre/fs3/portfolios/nvr/users/ymingli/experiments/mcmc/${LOC}_1mInit_Train200k_noDensify_McmcNoiseScale${NOISE_SCALE}_capMax${CAP_MAX}_GPU${GPU_NUM}_OpacityReg${OPACITY_REG}_ScaleReg${SCALE_REG} \
+ --iterations 200_000 --densify_until_iter 100_000 --mcmc --mcmc_noise_scale $NOISE_SCALE --cap_max $CAP_MAX --enable_timer --end2end_time --check_gpu_memory --check_cpu_memory --preload_dataset_to_gpu_threshold 0 --opacity_reg $OPACITY_REG --scale_reg $SCALE_REG --densification_interval $DENSIFY_INTER --densify_from_iter 9999999999999
+ 
+python render.py -s /lustre/fs3/portfolios/nvr/users/ymingli/datasets/Ithaca-full/rectified/$LOC -m /lustre/fs3/portfolios/nvr/users/ymingli/experiments/mcmc/${LOC}_1mInit_Train200k_noDensify_McmcNoiseScale${NOISE_SCALE}_capMax${CAP_MAX}_GPU${GPU_NUM}_OpacityReg${OPACITY_REG}_ScaleReg${SCALE_REG}
+
+/lustre/fsw/portfolios/nvr/users/ymingli/rclone copy /lustre/fs3/portfolios/nvr/users/ymingli/experiments/mcmc/${LOC}_1mInit_Train200k_noDensify_McmcNoiseScale${NOISE_SCALE}_capMax${CAP_MAX}_GPU${GPU_NUM}_OpacityReg${OPACITY_REG}_ScaleReg${SCALE_REG}/train/ours_199993/renders.mp4 gw2396:mcmc/${LOC}_1mInit_Train200k_noDensify_McmcNoiseScale${NOISE_SCALE}_capMax${CAP_MAX}_GPU${GPU_NUM}_OpacityReg${OPACITY_REG}_ScaleReg${SCALE_REG} -P
