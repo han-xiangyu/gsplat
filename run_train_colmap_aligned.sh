b8df7cb280f151d3ee7dceab9dabfa4fc303a1a0
MoonjunGong <moonjungong@gmail.com>
2024-08-25 15:21:36 +0800

    colmap aligned

--- run_train_colmap_aligned.sh
+++ run_train_colmap_aligned.sh
@@ -17,9 +17,12 @@ DENSIFY_FROM=2000
 DENSIFY_UNTIL=200000
 DENSIFY_INTER=100
 SCALE_LR=0.005
+# SCALE_LR=0.0005
 OPACITY_RESET=3000
 POS_LR=0.00016
 POS_LR_FINAL=0.0000016
+# POS_LR=0.00002
+# POS_LR_FINAL=0.0000002
 torchrun --standalone --nnodes=1 --nproc_per_node ${GPU_NUM} train.py --bsz ${GPU_NUM} -s /lustre/fs3/portfolios/nvr/users/ymingli/datasets/ithaca_colmap_aligned/$LOC -m /lustre/fs3/portfolios/nvr/users/ymingli/experiments/mcmc_colmap_aligned/${LOC}_ColmapInit_Train${ITER}_DensifyFrom${DENSIFY_FROM}DensifyUntil${DENSIFY_UNTIL}_DensifyInterval${DENSIFY_INTER}_McmcNoiseScale${NOISE_SCALE}_capMax${CAP_MAX}_GPU${GPU_NUM}_OpacityReg${OPACITY_REG}_ScaleReg${SCALE_REG}_PosLr${PS_LR}Scheduler${ITER}_ScaleLR${SCALE_LR}_OpacityReset${OPACITY_RESET}  \
  --iterations $ITER  --densify_from_iter $DENSIFY_FROM --densify_until_iter $DENSIFY_UNTIL --mcmc --mcmc_noise_scale $NOISE_SCALE --cap_max $CAP_MAX --enable_timer --end2end_time --check_gpu_memory --check_cpu_memory --preload_dataset_to_gpu_threshold 0 --opacity_reg $OPACITY_REG --scale_reg $SCALE_REG --densification_interval $DENSIFY_INTER --position_lr_init $POS_LR --position_lr_final $POS_LR_FINAL --position_lr_max_steps $ITER --scaling_lr $SCALE_LR --opacity_reset_interval $OPACITY_RESET
  
