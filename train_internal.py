80f6e6d92881fd2c0617cff3110cfdc4c7af0761
Xiangyu Han <xiangyu.han@outlook.com>
2025-06-10 16:30:48 +0800

    update HPC version

--- train_internal.py
+++ train_internal.py
@@ -48,6 +48,7 @@ def training(dataset_args, opt_args, pipe_args, args, log_file, WORLD_SIZE):
     # Logging into wandb
     if utils.DEFAULT_GROUP.rank() == 0:
         wandb.init(
+            entity="xiangyu-han",
             project=args.project_name or "CityGS",
             name=args.experiment_name or None,
             config={
