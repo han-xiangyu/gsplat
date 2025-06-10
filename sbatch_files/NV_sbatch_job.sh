a5c54498c071d1adb8e10f254f4e071c8fd89276
Xiangyu Han <xiangyu.han@outlook.com>
2025-06-10 19:25:52 +0800

    update HPC version

--- sbatch_files/NV_sbatch_job.sh
+++ sbatch_files/NV_sbatch_job.sh
@@ -3,7 +3,7 @@ job_name="citygs_train"
 training_logs_dir="/lustre/fsw/portfolios/nvr/users/ymingli/xiangyu/logs/citygs_train"
 
 submit_job --gpu 1 --cpu 16 --nodes 1 --partition=grizzly,polar,polar3,polar4 --account=nvr_av_end2endav \
-                --image = /lustre/fsw/portfolios/nvr/users/ymingli/dockers/cu118.sqsh  \ 
+                --image=/lustre/fsw/portfolios/nvr/users/ymingli/dockers/cu118.sqsh  \ 
                 --container-mounts=/lustre/:/lustre/,/lustre/fsw/portfolios/nvr/users/ymingli/miniconda3:/home/ymingli/miniconda3 \
                 --duration 4 \
                 --dependency=singleton \
