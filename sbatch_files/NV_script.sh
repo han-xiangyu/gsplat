024c12797f964b8b3ce1c5f33e92e4e9c819332d
Xiangyu Han <xiangyu.han@outlook.com>
2025-06-10 17:02:44 +0800

    update HPC version

--- sbatch_files/NV_script.sh
+++ sbatch_files/NV_script.sh
@@ -19,6 +19,8 @@ INIT_TYPE=sfm
 RANDOM_INIT_NUM=1000000
 
 export WANDB_API_KEY=9700db021b335e724b1c96fef3f087b458aff31e
+export WANDB_PROJECT=CityGS_xiangyu_test
+export WANDB_ENTITY=xiangyu-han
 
 torchrun --standalone --nnodes=1 --nproc_per_node ${GPU_NUM} train.py --bsz ${GPU_NUM} \
             -s /lustre/fsw/portfolios/nvr/users/ymingli/xiangyu/data/long_video_processed \
