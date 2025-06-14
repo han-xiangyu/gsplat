5dcc83a523b522c88c60d5d07ea1a684607f4049
Xiangyu Han <xiangyu.han@outlook.com>
2025-06-14 19:51:05 +0800

    update HPC version

--- sbatch_files/NV_sbatch_job.sh
+++ sbatch_files/NV_sbatch_job.sh
@@ -2,7 +2,7 @@
 job_name="3DfoundationModel"
 training_logs_dir="/lustre/fsw/portfolios/nvr/users/ymingli/gaussian/logs/citygs_partial_6000_downsample"
 
-submit_job --gpu 8 --cpu 24 --nodes 1 --partition=grizzly,polar,polar3,polar4 --account=nvr_av_end2endav \
+submit_job --gpu 1 --cpu 24 --nodes 1 --partition=grizzly,polar,polar3,polar4 --account=nvr_av_end2endav \
                 --image=/lustre/fsw/portfolios/nvr/users/ymingli/dockers/cu118.sqsh  \
                 --mounts=/lustre/:/lustre/,/lustre/fsw/portfolios/nvr/users/ymingli/miniconda3:/home/ymingli/miniconda3 \
                 --duration 4 \
