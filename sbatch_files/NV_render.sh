322886c68fdb79250b546c067bcfcb45c71c27b7
Xiangyu Han <xiangyu.han@outlook.com>
2025-06-18 16:12:17 +0800

    update sbatch command

---
+++ sbatch_files/NV_render.sh
@@ -0,0 +1,25 @@
+# NV cluster script
+
+# source /home/ymingli/miniconda3/bin/activate
+source /lustre/fs12/portfolios/nvr/users/ymingli/miniconda3/etc/profile.d/conda.sh
+conda activate citygs
+
+# Force to shield site-packages
+export PYTHONNOUSERSITE=1
+# Clear PYTHONPATH and  LD_LIBRARY_PATH
+unset PYTHONPATH
+export LD_LIBRARY_PATH=/usr/local/cuda-11.8/lib64:/usr/lib/x86_64-linux-gnu
+
+# Debug
+export CUDA_LAUNCH_BLOCKING=1
+export TORCH_USE_CUDA_DSA=1 
+
+# wandb configuration
+export WANDB_API_KEY=9700db021b335e724b1c96fef3f087b458aff31e
+export WANDB_MODE=disabled
+
+
+cd /lustre/fsw/portfolios/nvr/users/ymingli/gaussian/code/citygs
+
+
+python render.py -s /lustre/fsw/portfolios/nvr/users/ymingli/gaussian/data/long_video_processed_frames12000_pts_downsample  --model_path /lustre/fsw/portfolios/nvr/users/ymingli/gaussian/models/long_video_frames12000_full_distributed8GPU
--- sbatch_files/NV_sbatch_job.sh
+++ sbatch_files/NV_sbatch_job.sh
@@ -1,19 +1,35 @@
 
-job_name="3DfoundationModelDistributed8GPU"
-base_logdir="/lustre/fsw/portfolios/nvr/users/ymingli/gaussian/logs/citygs_partial_12000_full_8GPU"
+# job_name="3DfoundationModelDistributed8GPU"
+# base_logdir="/lustre/fsw/portfolios/nvr/users/ymingli/gaussian/logs/citygs_partial_12000_full_8GPU"
 
 
-for i in {1..1}; do
-    submit_job --gpu 8 --cpu 24 --nodes 1 --partition=grizzly,polar,polar3,polar4 --account=nvr_av_end2endav \
-                    --image=/lustre/fsw/portfolios/nvr/users/ymingli/dockers/cu118.sqsh  \
-                    --mounts=/lustre/:/lustre/,/lustre/fsw/portfolios/nvr/users/ymingli/miniconda3:/home/ymingli/miniconda3 \
-                    --duration 4 \
-                    --dependency=singleton \
-                    --name $job_name \
-                    --logdir ${base_logdir}/run_${i} \
-                    --notimestamp \
-                    --exclusive \
-                    --command  "bash /lustre/fsw/portfolios/nvr/users/ymingli/gaussian/code/citygs/sbatch_files/NV_run.sh"
-done
+# # Train
+# for i in {1..1}; do
+#     submit_job --gpu 8 --cpu 24 --nodes 1 --partition=grizzly,polar,polar3,polar4 --account=nvr_av_end2endav \
+#                     --image=/lustre/fsw/portfolios/nvr/users/ymingli/dockers/cu118.sqsh  \
+#                     --mounts=/lustre/:/lustre/,/lustre/fsw/portfolios/nvr/users/ymingli/miniconda3:/home/ymingli/miniconda3 \
+#                     --duration 4 \
+#                     --dependency=singleton \
+#                     --name $job_name \
+#                     --logdir ${base_logdir}/run_${i} \
+#                     --notimestamp \
+#                     --exclusive \
+#                     --command  "bash /lustre/fsw/portfolios/nvr/users/ymingli/gaussian/code/citygs/sbatch_files/NV_train.sh"
+# done
 
 # --copy_user_code /lustre/fsw/portfolios/nvr/users/ymingli/gaussian/code/citygs:code:[*] \
+
+job_name="3DfoundationModelRender12000"
+base_logdir="/lustre/fsw/portfolios/nvr/users/ymingli/gaussian/logs/citygs_partial_12000_full_render"
+# Render
+
+submit_job --gpu 1 --cpu 48 --nodes 1 --partition=grizzly,polar,polar3,polar4 --account=nvr_av_end2endav \
+                --image=/lustre/fsw/portfolios/nvr/users/ymingli/dockers/cu118.sqsh  \
+                --mounts=/lustre/:/lustre/,/lustre/fsw/portfolios/nvr/users/ymingli/miniconda3:/home/ymingli/miniconda3 \
+                --duration 4 \
+                --dependency=singleton \
+                --name $job_name \
+                --logdir $base_logdir \
+                --notimestamp \
+                --command  "bash /lustre/fsw/portfolios/nvr/users/ymingli/gaussian/code/citygs/sbatch_files/NV_render.sh"
+
