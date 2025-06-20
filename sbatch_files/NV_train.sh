dc1cdbd54307d657b45c5b22a8aac342987e5fa8
Xiangyu Han <xiangyu.han@outlook.com>
2025-06-20 16:47:00 +0800

    update sbatch

--- sbatch_files/NV_train.sh
+++ sbatch_files/NV_train.sh
@@ -18,7 +18,7 @@ export TORCH_USE_CUDA_DSA=1
 
 
 # Configuration
-GPU_NUM=8
+GPU_NUM=2
 CAP_MAX=5000000
 NOISE_SCALE=500000
 OPACITY_REG=0
@@ -41,36 +41,36 @@ model_name=$(basename "$MODEL_PATH")
 
 # wandb configuration
 export WANDB_API_KEY=9700db021b335e724b1c96fef3f087b458aff31e
-# export WANDB_MODE=disabled
+export WANDB_MODE=disabled
 PROJECT_NAME=CityGS_long_video
 # EXPERIENT_NAME="${source_name}_cap${CAP_MAX}_opacityREG${OPACITY_REG}_scaleLR${SCALE_LR}_opacityLR005_posLR${POS_LR}_posLRfinal${POS_LR_FINAL}_densifyFrom${DENSIFY_FROM}Final${DENSIFY_UNTIL}Iter${ITER}"
 EXPERIENT_NAME=$model_name
 video_output_path="${MODEL_PATH}/${model_name}_train_set_video.mp4"
 remote_video_name="${source_name}_$(date +%m%d_%H%M).mp4"
 
-torchrun --standalone --nnodes=1 --nproc_per_node ${GPU_NUM} train.py --bsz ${GPU_NUM} \
-            -s $SOURCE_PATH \
-            -m $MODEL_PATH \
-            --iterations $ITER  \
-            --densify_from_iter $DENSIFY_FROM \
-            --densify_until_iter $DENSIFY_UNTIL \
-            --densification_interval $DENSIFY_INTER \
-            --cap_max $CAP_MAX \
-            --enable_timer --end2end_time --check_gpu_memory --check_cpu_memory --preload_dataset_to_gpu_threshold 0 \
-            --opacity_reg $OPACITY_REG \
-            --scale_reg $SCALE_REG \
-            --position_lr_init $POS_LR \
-            --position_lr_final $POS_LR_FINAL \
-            --position_lr_max_steps $ITER \
-            --scaling_lr $SCALE_LR \
-            --opacity_reset_interval $OPACITY_RESET \
-            --init_type $INIT_TYPE \
-            --experiment_name  $EXPERIENT_NAME\
-            --project_name $PROJECT_NAME \
-            --auto_start_checkpoint \
-            --mcmc --mcmc_noise_scale $NOISE_SCALE 
-            # --backend gsplat \
-            # --resolution 4 \
+# torchrun --standalone --nnodes=1 --nproc_per_node ${GPU_NUM} train.py --bsz ${GPU_NUM} \
+#             -s $SOURCE_PATH \
+#             -m $MODEL_PATH \
+#             --iterations $ITER  \
+#             --densify_from_iter $DENSIFY_FROM \
+#             --densify_until_iter $DENSIFY_UNTIL \
+#             --densification_interval $DENSIFY_INTER \
+#             --cap_max $CAP_MAX \
+#             --enable_timer --end2end_time --check_gpu_memory --check_cpu_memory --preload_dataset_to_gpu_threshold 0 \
+#             --opacity_reg $OPACITY_REG \
+#             --scale_reg $SCALE_REG \
+#             --position_lr_init $POS_LR \
+#             --position_lr_final $POS_LR_FINAL \
+#             --position_lr_max_steps $ITER \
+#             --scaling_lr $SCALE_LR \
+#             --opacity_reset_interval $OPACITY_RESET \
+#             --init_type $INIT_TYPE \
+#             --experiment_name  $EXPERIENT_NAME\
+#             --project_name $PROJECT_NAME \
+#             --auto_start_checkpoint \
+#             --mcmc --mcmc_noise_scale $NOISE_SCALE 
+#             # --backend gsplat \
+#             # --resolution 4 \
 
 
 torchrun --nproc_per_node=${GPU_NUM} render.py --distributed_load -s $SOURCE_PATH  --model_path $MODEL_PATH
