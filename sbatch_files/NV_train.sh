3b0c26b57720fe1fd313e87ab610ae4101312bb7
Xiangyu Han <xiangyu.han@outlook.com>
2025-06-21 19:22:48 +0800

    update sbatch

--- sbatch_files/NV_train.sh
+++ sbatch_files/NV_train.sh
@@ -34,8 +34,8 @@ POS_LR_FINAL=2e-5
 INIT_TYPE=sfm
 
 
-SOURCE_PATH=/lustre/fsw/portfolios/nvr/users/ymingli/gaussian/data/data_process_spatial05_frames6000
-MODEL_PATH=/lustre/fsw/portfolios/nvr/users/ymingli/gaussian/model/data_process_spatial05_frames6000
+SOURCE_PATH=/lustre/fsw/portfolios/nvr/users/ymingli/gaussian/data/spatial05_frames6000
+MODEL_PATH=/lustre/fsw/portfolios/nvr/users/ymingli/gaussian/model/spatial05_frames6000
 source_name=$(basename "$SOURCE_PATH")
 model_name=$(basename "$MODEL_PATH")
 
