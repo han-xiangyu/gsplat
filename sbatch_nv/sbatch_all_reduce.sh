ca0dafa1223b816b71e3192c3ba66a79fae11cbd
Bowen Chang <133490216+KarlCbw@users.noreply.github.com>
2025-10-16 15:21:30 +0800

    [fix] fix all reduce script

--- sbatch_nv/sbatch_all_reduce.sh
+++ sbatch_nv/sbatch_all_reduce.sh
@@ -1,11 +1,11 @@
-gpus_per_node=8
+gpus_per_node=1
 nodes=2
 account=nvr_av_foundations
 
 submit_job --more_srun_args=--gpus-per-node=$gpus_per_node --nodes $nodes \
     --partition=grizzly,polar \
     --account=$account \
-    --duration 0.2 \
+    --duration 0.5 \
     --exclusive \
     --logroot=/lustre/fsw/portfolios/nvr/users/ymingli/datasets/citygs/log/allreduce \
     --image=/lustre/fsw/portfolios/nvr/users/ymingli/dockers/2304py3.sqsh \
