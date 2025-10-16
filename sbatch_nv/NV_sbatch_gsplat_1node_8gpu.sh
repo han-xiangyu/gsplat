adfa82ef55926334ca1bc5b764ed4d7dd12fca97
Bowen Chang <133490216+KarlCbw@users.noreply.github.com>
2025-10-16 19:06:24 +0800

    [update] update train sbatch of 6cam

--- sbatch_nv/NV_sbatch_gsplat_1node_8gpu.sh
+++ sbatch_nv/NV_sbatch_gsplat_1node_8gpu.sh
@@ -1,16 +1,18 @@
-job_name="gsplat_1node_8gpu_6cam_voxel"
-base_logdir="/lustre/fsw/portfolios/nvr/users/ymingli/datasets/citygs/log/gsplat_1node_8gpu_6cam_voxel"
+job_name="gsplat_6cam"
+base_logdir="/lustre/fsw/portfolios/nvr/users/ymingli/datasets/citygs/log/gsplat_6cam"
 account=nvr_av_foundations
 
-submit_job --gpu 8 --cpu 16 --nodes 1 \
-    --partition=grizzly,polar,polar3,polar4 \
-    --account=$account \
-    --image=/lustre/fsw/portfolios/nvr/users/ymingli/dockers/2304py3.sqsh \
-    --mounts=/lustre/:/lustre/,/lustre/fsw/portfolios/nvr/users/ymingli/miniconda3:/home/ymingli/miniconda3 \
-    --duration 4 \
-    --dependency=singleton \
-    --name $job_name \
-    --logdir ${base_logdir} \
-    --exclusive \
-    --command "bash /lustre/fsw/portfolios/nvr/users/ymingli/gaussian/code/gsplat/sbatch_nv/NV_train_origin_1node_8gpu.sh"
+for i in {1..2}; do
+    submit_job --gpu 8 --cpu 16 --nodes 1 \
+        --partition=grizzly,polar,polar3,polar4 \
+        --account=$account \
+        --image=/lustre/fsw/portfolios/nvr/users/ymingli/dockers/2304py3.sqsh \
+        --mounts=/lustre/:/lustre/,/lustre/fsw/portfolios/nvr/users/ymingli/miniconda3:/home/ymingli/miniconda3 \
+        --duration 4 \
+        --dependency=singleton \
+        --name $job_name \
+        --logdir ${base_logdir} \
+        --exclusive \
+        --command "bash /lustre/fsw/portfolios/nvr/users/ymingli/gaussian/code/gsplat/sbatch_nv/NV_train_origin_1node_8gpu.sh"
+done
 
