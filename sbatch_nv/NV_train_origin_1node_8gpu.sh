7a2e86115e6c048dfd8354106316053bc3af4945
Bowen Chang <133490216+KarlCbw@users.noreply.github.com>
2025-10-19 19:39:59 +0800

    [update] update 2000keyframes configs

--- sbatch_nv/NV_train_origin_1node_8gpu.sh
+++ sbatch_nv/NV_train_origin_1node_8gpu.sh
@@ -2,30 +2,34 @@
 source /lustre/fsw/portfolios/nvr/users/ymingli/miniconda3/bin/activate
 conda activate mars_pytorh3d
 cd /lustre/fsw/portfolios/nvr/users/ymingli/projects/gsplat-city/submodules/gsplat
-SOURCE_PATH="/lustre/fsw/portfolios/nvr/users/ymingli/datasets/citygs/data/tra2_spatial05_frames15000_individual_K_voxel_3cam"
-#SOURCE_PATH="/lustre/fsw/portfolios/nvr/users/ymingli/gaussian/data/spatial05_frames9000_individual_K"
-MODEL_PATH="/lustre/fsw/portfolios/nvr/users/ymingli/datasets/citygs/models/tra2_spatial05_frames15000_individual_K_voxel_3cam"
+
+# configs
+NUM_CAMS=3
+NUM_KEYFRAMES=2000
+TRAVERSAL_ID=2
+DOWNSAMPLE_TYPE="fps"
+
+BASE_DIR="/lustre/fsw/portfolios/nvr/users/ymingli/datasets/citygs"
+SOURCE_PATH="${BASE_DIR}/data/tra${TRAVERSAL_ID}_${NUM_KEYFRAMES}keyframes_${DOWNSAMPLE_TYPE}_${NUM_CAMS}cam"
+MODEL_PATH="${BASE_DIR}/models/tra${TRAVERSAL_ID}_${NUM_KEYFRAMES}keyframes_${DOWNSAMPLE_TYPE}_${NUM_CAMS}cam"
 model_name=$(basename "$MODEL_PATH")
 export CUDA_LAUNCH_BLOCKING=1
 export TORCH_USE_CUDA_DSA=1
 export PYTHONPATH=$PYTHONPATH:/lustre/fsw/portfolios/nvr/users/ymingli/projects/gsplat-city/submodules/gsplat
 export PYTHONPATH=$PYTHONPATH:/lustre/fsw/portfolios/nvr/users/ymingli/projects/gsplat-city/submodules/gsplat/pycolmap
-export WANDB_DIR="/lustre/fsw/portfolios/nvr/users/ymingli/datasets/citygs/wandb_logs/${model_name}"
+export WANDB_DIR="${BASE_DIR}/wandb_logs/${model_name}"
 export WANDB_API_KEY=42e7b9b31273e3a7a2bc3527a0784472e70848a2
 export WANDB_INSECURE_DISABLE_SSL=true
 export WANDB_SILENT=true
 
 PROJECT_NAME=gsplat_ablation
 EXPERIENT_NAME=$model_name
-video_output_path="${MODEL_PATH}/videos/traj_149999.mp4"
-remote_video_name="${model_name}_$(date +%m%d_%H%M)"
 max_steps=150_000
 MEANS_LR=2e-3
 MEAN_LR_FINAL_MULT=1e-4
 densify_portion=0.001
 depth_lambda=2e-3
 pose_opt_start=1e5
-# --use_bilateral_grid
 export PYTHONWARNINGS="ignore:The pynvml package is deprecated"
 
 torchrun --standalone \
@@ -38,7 +42,7 @@ torchrun --standalone \
      --wandb_project=$PROJECT_NAME \
      --wandb_group=gsplat \
      --wandb_name=$EXPERIENT_NAME \
-     --wandb_mode='offline' \
+     --wandb_mode='online' \
      --wandb_dir=$WANDB_DIR \
      --wandb_log_images_every=50000 \
      --means_lr $MEANS_LR \
@@ -53,11 +57,11 @@ torchrun --standalone \
      --strategy.schedule-mode='original' \
      --strategy.densify_portion $densify_portion \
 
-# echo "Training finished. Starting rendering ..."
-# python examples/render_from_ply.py \
-#      --data_dir $SOURCE_PATH \
-#      --ply_path $MODEL_PATH/ply/point_cloud_199999.ply  \
-#      --result_dir $MODEL_PATH \
-#      --fps 15 \
-#      --channels 2 1 3
+echo "Training finished. Starting rendering ..."
+python examples/render_from_ply.py \
+     --data_dir $SOURCE_PATH \
+     --ply_path $MODEL_PATH/ply/point_cloud_149999.ply  \
+     --result_dir $MODEL_PATH \
+     --fps 15 \
+     --channels 2 1 3
 
