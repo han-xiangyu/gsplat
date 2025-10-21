f593bd9559a01348e2ca2d5f53f95680486a5ed7
Bowen Chang <133490216+KarlCbw@users.noreply.github.com>
2025-10-21 10:59:06 +0800

    [fix] fix script bug

--- sbatch_nv/NV_sbatch_gsplat.sh
+++ sbatch_nv/NV_sbatch_gsplat.sh
@@ -1,7 +1,7 @@
 job_name="tra2_3000keyframes_fps_3cam_v2"
 base_logdir="/lustre/fsw/portfolios/nvr/users/ymingli/datasets/citygs/log/tra2_3000keyframes_fps_3cam_v2"
 
-for i in {4..6}; do
+for i in {5..6}; do
     submit_job --gpu 8 --cpu 16 --nodes 1 \
         --partition=grizzly,polar,polar3,polar4 \
         --account=nvr_av_end2endav \
