55c8efaf2cf1b6d2ae13e58ac9b25f7c88122e06
Bowen Chang <133490216+KarlCbw@users.noreply.github.com>
2025-10-24 02:48:05 +0800

    [fix] fix bug

---
+++ sbatch_nv/NV_sbatch_b1_gpsnew.sh
@@ -0,0 +1,17 @@
+job_name="tra2_3000keyframes_fps8w_3cam_gpsnew"
+base_logdir="/lustre/fsw/portfolios/nvr/users/ymingli/datasets/citygs/log/tra2_3000keyframes_fps8w_3cam_gpsnew"
+
+for i in {1..5}; do
+    submit_job --gpu 8 --cpu 16 --nodes 1 \
+    --partition=grizzly,polar,polar3,polar4 \
+    --account=nvr_av_foundations \
+    --image=/lustre/fsw/portfolios/nvr/users/ymingli/dockers/2304py3.sqsh \
+    --mounts=/lustre/:/lustre/,/lustre/fsw/portfolios/nvr/users/ymingli/miniconda3:/home/ymingli/miniconda3 \
+    --duration 4 \
+    --dependency=singleton \
+    --name ${job_name} \
+    --logdir ${base_logdir}/run_${i} \
+    --notimestamp \
+    --exclusive \
+    --command "bash /lustre/fsw/portfolios/nvr/users/ymingli/gaussian/code/gsplat/sbatch_nv/NV_train_b1_gpsnew.sh"
+done
