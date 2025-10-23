c6ffa145ea4c3033e04f4e081ba2e16000102089
Bowen Chang <133490216+KarlCbw@users.noreply.github.com>
2025-10-23 19:17:23 +0800

    [update] add dynamic mask training scripts

--- sbatch_nv/NV_train_b1_dmask.sh
+++ sbatch_nv/NV_train_b1_dmask.sh
@@ -10,8 +10,8 @@ TRAVERSAL_ID=2
 DOWNSAMPLE_TYPE="fps"
 
 BASE_DIR="/lustre/fsw/portfolios/nvr/users/ymingli/datasets/citygs"
-SOURCE_PATH="${BASE_DIR}/data/tra${TRAVERSAL_ID}_3000keyframes_${DOWNSAMPLE_TYPE}_${NUM_CAMS}cam_gps"
-MODEL_PATH="${BASE_DIR}/models/tra${TRAVERSAL_ID}_3000keyframes_${DOWNSAMPLE_TYPE}_${NUM_CAMS}cam_gps"
+SOURCE_PATH="${BASE_DIR}/data/tra${TRAVERSAL_ID}_3000keyframes_${DOWNSAMPLE_TYPE}_${NUM_CAMS}cam"
+MODEL_PATH="${BASE_DIR}/models/tra${TRAVERSAL_ID}_3000keyframes_${DOWNSAMPLE_TYPE}_${NUM_CAMS}cam_dmask"
 model_name=$(basename "$MODEL_PATH")
 export CUDA_LAUNCH_BLOCKING=1
 export TORCH_USE_CUDA_DSA=1
