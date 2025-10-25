1c74cb715caf2071464343bfb6398e44c9ffb807
Bowen Chang <133490216+KarlCbw@users.noreply.github.com>
2025-10-25 14:45:02 +0800

    [update] update 3000to6000keyframes training configs with dynamic mask

--- sbatch_nv/NV_sbatch_b2_dmask.sh
+++ sbatch_nv/NV_sbatch_b2_dmask.sh
@@ -1,10 +1,10 @@
-job_name="newpose_tra2_3000keyframes_fps_3cam"
-base_logdir="/lustre/fsw/portfolios/nvr/users/ymingli/datasets/citygs/log/newpose_tra2_3000keyframes_fps_3cam"
+job_name="dynamicmask_tra2_3000to6000keyframes_fps_3cam"
+base_logdir="/lustre/fsw/portfolios/nvr/users/ymingli/datasets/citygs/log/dynamicmask_tra2_3000to6000keyframes_fps_3cam"
 
 for i in {1..5}; do
     submit_job --gpu 8 --cpu 16 --nodes 1 \
         --partition=grizzly,polar,polar3,polar4 \
-        --account=nvr_av_end2endav \
+        --account=nvr_av_foundations \
         --image=/lustre/fsw/portfolios/nvr/users/ymingli/dockers/2304py3.sqsh \
         --mounts=/lustre/:/lustre/,/lustre/fsw/portfolios/nvr/users/ymingli/miniconda3:/home/ymingli/miniconda3 \
         --duration 4 \
@@ -13,5 +13,5 @@ for i in {1..5}; do
         --logdir ${base_logdir}/run_${i} \
         --notimestamp \
         --exclusive \
-        --command "bash /lustre/fsw/portfolios/nvr/users/ymingli/gaussian/code/gsplat/sbatch_nv/NV_train_b1_newpose.sh"
+        --command "bash /lustre/fsw/portfolios/nvr/users/ymingli/gaussian/code/gsplat/sbatch_nv/NV_train_b2_dmask.sh"
 done
