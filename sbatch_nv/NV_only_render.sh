2aa5612c21d1203c129a3a3b67dc1a5f37dd73e8
Bowen Chang <133490216+KarlCbw@users.noreply.github.com>
2025-10-25 17:27:35 +0800

    [fix] fix bug

--- sbatch_nv/NV_only_render.sh
+++ sbatch_nv/NV_only_render.sh
@@ -2,7 +2,7 @@
 source /lustre/fsw/portfolios/nvr/users/ymingli/miniconda3/bin/activate
 conda activate mars_pytorh3d
 cd /lustre/fsw/portfolios/nvr/users/ymingli/projects/gsplat-city/submodules/gsplat
-SOURCE_PATH="/lustre/fsw/portfolios/nvr/users/ymingli/datasets/citygs/data/tra2_3000keyframes_fps_3cam_dynamicmask"
+SOURCE_PATH="/lustre/fsw/portfolios/nvr/users/ymingli/datasets/citygs/data/tra2_3000keyframes_fps_3cam"
 MODEL_PATH="/lustre/fsw/portfolios/nvr/users/ymingli/datasets/citygs/models/tra2_3000keyframes_fps_3cam_dynamicmask"
 model_name=$(basename "$MODEL_PATH")
 export CUDA_LAUNCH_BLOCKING=1
