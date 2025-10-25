00b206f93b0fce5a8d03b531a72efbcb415e501c
Bowen Chang <133490216+KarlCbw@users.noreply.github.com>
2025-10-25 17:24:49 +0800

    [update] update only render script

--- sbatch_nv/NV_only_render.sh
+++ sbatch_nv/NV_only_render.sh
@@ -2,8 +2,8 @@
 source /lustre/fsw/portfolios/nvr/users/ymingli/miniconda3/bin/activate
 conda activate mars_pytorh3d
 cd /lustre/fsw/portfolios/nvr/users/ymingli/projects/gsplat-city/submodules/gsplat
-SOURCE_PATH="/lustre/fsw/portfolios/nvr/users/ymingli/datasets/citygs/data/tra2_3000to6000keyframes_fps_3cam"
-MODEL_PATH="/lustre/fsw/portfolios/nvr/users/ymingli/datasets/citygs/models/tra2_3000to6000keyframes_fps_3cam"
+SOURCE_PATH="/lustre/fsw/portfolios/nvr/users/ymingli/datasets/citygs/data/tra2_3000keyframes_fps_3cam_dynamicmask"
+MODEL_PATH="/lustre/fsw/portfolios/nvr/users/ymingli/datasets/citygs/models/tra2_3000keyframes_fps_3cam_dynamicmask"
 model_name=$(basename "$MODEL_PATH")
 export CUDA_LAUNCH_BLOCKING=1
 export TORCH_USE_CUDA_DSA=1
@@ -17,7 +17,7 @@ export PYTHONWARNINGS="ignore:The pynvml package is deprecated"
 echo "Training finished. Starting rendering ..."
 python examples/render_from_ply.py \
      --data_dir $SOURCE_PATH \
-     --ply_path $MODEL_PATH/ply/point_cloud_99999.ply  \
+     --ply_path $MODEL_PATH/ply/point_cloud_149999.ply  \
      --result_dir $MODEL_PATH \
      --fps 15 \
      --channels 2 1 3
