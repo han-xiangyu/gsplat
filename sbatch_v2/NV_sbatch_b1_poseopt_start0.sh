133b310c867bb97663ee67fae58cc68f94f4152f
Bowen Chang <133490216+KarlCbw@users.noreply.github.com>
2025-10-28 23:33:49 +0800

    [fix] fix bug

---
+++ sbatch_v2/NV_sbatch_b1_poseopt_start0.sh
@@ -0,0 +1,17 @@
+job_name="start0_poseopt_3000keyframes_fps_3cam"
+base_logdir="/lustre/fsw/portfolios/nvr/users/ymingli/datasets/citygs/logv2/poseopt_3000keyframes_fps_3cam"
+
+for i in {1..5}; do
+    submit_job --gpu 8 --cpu 16 --nodes 1 \
+        --partition=grizzly,polar,polar3,polar4 \
+        --account=nvr_av_foundations \
+        --image=/lustre/fsw/portfolios/nvr/users/ymingli/dockers/2304py3.sqsh \
+        --mounts=/lustre/:/lustre/,/lustre/fsw/portfolios/nvr/users/ymingli/miniconda3:/home/ymingli/miniconda3 \
+        --duration 4 \
+        --dependency=singleton \
+        --name ${job_name} \
+        --logdir ${base_logdir}/run_${i} \
+        --notimestamp \
+        --exclusive \
+        --command "bash /lustre/fsw/portfolios/nvr/users/ymingli/gaussian/code/gsplat/sbatch_v2/NV_train_b1_poseopt_start0.sh"
+done
