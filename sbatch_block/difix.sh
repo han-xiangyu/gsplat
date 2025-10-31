8c9223a415541c61d5b649829465b4fc02359512
Bowen Chang <133490216+KarlCbw@users.noreply.github.com>
2025-10-31 10:48:44 +0800

    [fix] fix bug

---
+++ sbatch_block/difix.sh
@@ -0,0 +1,16 @@
+job_name="tra2_14000to15000keyframes_augment_difix3d"
+base_logdir="/lustre/fsw/portfolios/nvr/users/ymingli/datasets/citygs/log/tra2_14000to15000keyframes_augment_difix3d"
+
+
+submit_job --gpu 8 --cpu 16 --nodes 1 \
+    --partition=grizzly,polar,polar3,polar4 \
+    --account=nvr_av_foundations \
+    --image=/lustre/fsw/portfolios/nvr/users/ymingli/dockers/2304py3.sqsh \
+    --mounts=/lustre/:/lustre/,/lustre/fsw/portfolios/nvr/users/ymingli/miniconda3:/home/ymingli/miniconda3 \
+    --duration 3 \
+    --dependency=singleton \
+    --name ${job_name} \
+    --logdir ${base_logdir} \
+    --notimestamp \
+    --exclusive \
+    --command "bash /lustre/fsw/portfolios/nvr/users/ymingli/gaussian/code/gsplat/sbatch_nv/difix_14000-15000.sh"
