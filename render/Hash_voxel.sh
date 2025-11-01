1771fb1d389670d573f65bc5a9bd8843d016d188
Bowen Chang <133490216+KarlCbw@users.noreply.github.com>
2025-11-01 22:23:50 +0800

    [fix] fix bug

--- render/Hash_voxel.sh
+++ render/Hash_voxel.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 source /lustre/fsw/portfolios/nvr/users/ymingli/miniconda3/bin/activate
 conda activate mars_pytorh3d
-cd /lustre/fsw/portfolios/nvr/users/ymingli/projects/gsplat-city/submodules/gsplat
 NUM_CAMS=3
 TRAVERSAL_ID=2
 DOWNSAMPLE_TYPE="fps"
@@ -9,12 +8,13 @@ S=13000
 T1=14000
 E=15000
 BASE_DIR="/lustre/fsw/portfolios/nvr/users/ymingli/datasets/citygs"
+PROJECT_DIR="/lustre/fsw/portfolios/nvr/users/ymingli/gaussian/code/gsplat"
 SOURCE1="${BASE_DIR}/data/tra${TRAVERSAL_ID}_${S}to${T1}keyframes_${DOWNSAMPLE_TYPE}_${NUM_CAMS}cam_with_newviews"
 SOURCE2="${BASE_DIR}/data/tra${TRAVERSAL_ID}_${T1}to${E}keyframes_${DOWNSAMPLE_TYPE}_${NUM_CAMS}cam_with_newviews"
 MODEL1="${BASE_DIR}/models_block/tra${TRAVERSAL_ID}_${S}to${T1}keyframes_${DOWNSAMPLE_TYPE}_${NUM_CAMS}cam_with_newviews"
 MODEL2="${BASE_DIR}/models_block/tra${TRAVERSAL_ID}_${T1}to${E}keyframes_${DOWNSAMPLE_TYPE}_${NUM_CAMS}cam_with_newviews"
 
-python filter_gs.py \
+python $PROJECT_DIR/examples/filter_gs.py \
     --ply_in $MODEL1/ply/point_cloud_149999.ply \
     --lidar_in $SOURCE1/sparse/0/points3D.txt \
     --ply_out $MODEL1/ply/point_cloud_149999_cleaned.ply \
