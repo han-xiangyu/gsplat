388c129ea84e1983a7f0715cd60643d2406b51d8
Bowen Chang <133490216+KarlCbw@users.noreply.github.com>
2025-11-06 20:54:49 +0800

    add

--- sbatch_nv/mnode/2node_lr8.sh
+++ sbatch_nv/mnode/2node_lr8.sh
@@ -4,7 +4,7 @@ source /lustre/fsw/portfolios/nvr/users/ymingli/miniconda3/bin/activate
 conda activate mars_pytorh3d
 cd /lustre/fsw/portfolios/nvr/users/ymingli/projects/gsplat-city/submodules/gsplat
 SOURCE_PATH="/lustre/fsw/portfolios/nvr/users/ymingli/datasets/citygs/data_mnode/tra2_0to1000keyframes_fps_3cam"
-MODEL_PATH="/lustre/fsw/portfolios/nvr/users/ymingli/datasets/citygs/models_mnode/tra2_0to1000keyframes_fps_3cam_2node_wo_selectiveadam"
+MODEL_PATH="/lustre/fsw/portfolios/nvr/users/ymingli/datasets/citygs/models_mnode/tra2_0to1000keyframes_fps_3cam_2node_w_visible_adam"
 model_name=$(basename "$MODEL_PATH")
 export CUDA_LAUNCH_BLOCKING=1
 export TORCH_USE_CUDA_DSA=1
