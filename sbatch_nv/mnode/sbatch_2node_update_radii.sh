565e0f58aac5bb3968dc06a3dd3cdba8485aff9f
Bowen Chang <133490216+KarlCbw@users.noreply.github.com>
2025-11-07 01:02:36 +0800

    [fix] fix radii update in rasterization

---
+++ sbatch_nv/mnode/sbatch_2node_update_radii.sh
@@ -0,0 +1,18 @@
+job_name="2n16g_update_radii_citygs"
+logdir="/lustre/fsw/portfolios/nvr/users/ymingli/datasets/citygs/log_mnode/2n16g_update_radii_citygs"
+gpus_per_node=8
+nodes=2
+account=nvr_av_foundations
+submit_job --more_srun_args=--gpus-per-node=$gpus_per_node --nodes $nodes \
+    --partition=grizzly,polar \
+    --account=$account \
+    --duration 4 \
+    --exclusive \
+    --logroot=$logdir \
+    --image=/lustre/fsw/portfolios/nvr/users/ymingli/dockers/2304py3.sqsh \
+    --name=$job_name \
+    --dependency=singleton \
+    --command "bash /lustre/fsw/portfolios/nvr/users/ymingli/gaussian/code/gsplat/sbatch_nv/mnode/2node_lr8_update_radii.sh" \
+    --notimestamp \
+    --email_mode never \
+    --notification_mode never
