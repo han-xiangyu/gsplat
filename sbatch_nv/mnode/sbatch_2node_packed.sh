796e8826dae2d78ed90beed667c5489a11b7b249
Bowen Chang <133490216+KarlCbw@users.noreply.github.com>
2025-11-07 23:59:33 +0800

    [fix] fix bug

---
+++ sbatch_nv/mnode/sbatch_2node_packed.sh
@@ -0,0 +1,18 @@
+job_name="2node_8gpulr_citygs_packed"
+logdir="/lustre/fsw/portfolios/nvr/users/ymingli/datasets/citygs/log_mnode/2node_8gpulr_citygs_packed"
+gpus_per_node=8
+nodes=2
+account=nvr_av_end2endav
+submit_job --more_srun_args=--gpus-per-node=$gpus_per_node --nodes $nodes \
+    --partition=grizzly,polar \
+    --account=$account \
+    --duration 4 \
+    --exclusive \
+    --logroot=$logdir \
+    --image=/lustre/fsw/portfolios/nvr/users/ymingli/dockers/2304py3.sqsh \
+    --name=$job_name \
+    --dependency=singleton \
+    --command "bash /lustre/fsw/portfolios/nvr/users/ymingli/gaussian/code/gsplat/sbatch_nv/mnode/2node_lr8_packed.sh" \
+    --notimestamp \
+    --email_mode never \
+    --notification_mode never
