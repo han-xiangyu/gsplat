b2c1543be0846cd94a894b9b682978eecd87a901
Bowen Chang <133490216+KarlCbw@users.noreply.github.com>
2025-11-11 16:06:06 +0800

    [fix] fix bug

---
+++ sbatch_pose_opt/nv_base_single.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+#MODE="difix"
+MODE=""
+JOB_BASE_NAME=$(basename "$0" .sh)
+LOG_BASE_PREFIX="/lustre/fsw/portfolios/nvr/users/ymingli/datasets/citygs/log_poseopt"
+
+JOB_SUFFIX=""
+if [ "$MODE" == "difix" ]; then
+    echo "--- 'difix' is on ---"
+    JOB_SUFFIX="_difix"
+else
+    echo "--- 'standard' is on ---"
+fi
+
+job_name="${JOB_BASE_NAME}${JOB_SUFFIX}"
+base_logdir="${LOG_BASE_PREFIX}/${job_name}"
+
+echo "--- submit job ---"
+echo "Job Name: $job_name"
+echo "Log Dir:  $base_logdir"
+echo "Mode Arg: [${MODE:-standard}]"
+
+for i in {1..3}; do
+    submit_job --gpu 8 --cpu 16 --nodes 1 \
+        --partition=grizzly,polar,polar3,polar4 \
+        --account=nvr_av_end2endav \
+        --image=/lustre/fsw/portfolios/nvr/users/ymingli/dockers/2304py3.sqsh \
+        --mounts=/lustre/:/lustre/,/lustre/fsw/portfolios/nvr/users/ymingli/miniconda3:/home/ymingli/miniconda3 \
+        --duration 4 \
+        --dependency=singleton \
+        --name ${job_name} \
+        --logdir ${base_logdir}/run_${i} \
+        --notimestamp \
+        --exclusive \
+        --command "bash /lustre/fsw/portfolios/nvr/users/ymingli/gaussian/code/gsplat/pose_opt/base_single.sh $MODE"
+done
+
+echo "--- submit success! ---"
