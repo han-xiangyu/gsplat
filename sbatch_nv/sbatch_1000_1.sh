4d06fb124b1c0e758dda307bc66a7e30881ea37a
KarlCbw <changkarl4@gmail.com>
2025-12-07 01:22:39 -0500

    add

---
+++ sbatch_nv/sbatch_1000_1.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+#MODE="difix"
+MODE=""
+
+JOB_BASE_NAME="may_smallscene_origin"
+LOG_BASE_PREFIX="/lustre/fsw/portfolios/nvr/users/ymingli/datasets/citygs/log"
+
+JOB_SUFFIX=""
+if [ "$MODE" == "difix" ]; then
+    echo "--- 'difix' is on ---"
+    JOB_SUFFIX="_difix"
+else
+    echo "--- 'standard' is on ---"
+fi
+
+job_name="${JOB_BASE_NAME}${JOB_SUFFIX}"
+base_logdir="${LOG_BASE_PREFIX}/${job_name}"
+
+echo "--- submit job ---"
+echo "Job Name: $job_name"
+echo "Log Dir:  $base_logdir"
+echo "Mode Arg: [${MODE:-standard}]"
+
+for i in {1..2}; do
+    submit_job --gpu 8 --cpu 16 --nodes 1 \
+        --partition=grizzly,polar,polar3,polar4 \
+        --account=nvr_av_end2endav \
+        --image=/lustre/fsw/portfolios/nvr/users/ymingli/dockers/2304py3.sqsh \
+        --mounts=/lustre/:/lustre/,/lustre/fsw/portfolios/nvr/users/ymingli/miniconda3:/home/ymingli/miniconda3 \
+        --duration 4 \
+        --dependency=singleton \
+        --name ${job_name} \
+        --logdir ${base_logdir}/run_${i} \
+        --notimestamp \
+        --exclusive \
+        --command "bash /lustre/fsw/portfolios/nvr/users/ymingli/gaussian/code/gsplat/nv_train/small_origin.sh $MODE"
+done
+
+echo "--- Tasks have been submited! ---"
