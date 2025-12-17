5e1757d70b145601715319cb50de5172ee375ca2
KarlCbw <changkarl4@gmail.com>
2025-12-17 22:04:52 +0800

    ADD

--- nv_train/small_sky.sh
+++ nv_train/small_sky.sh
@@ -2,7 +2,7 @@
 source /lustre/fsw/portfolios/nvr/users/ymingli/miniconda3/bin/activate
 conda activate mars_new
 cd /lustre/fsw/portfolios/nvr/users/ymingli/projects/gsplat-city/submodules/gsplat
-
+DATE="1217_2203"
 BASE_DIR="/lustre/fsw/portfolios/nvr/users/ymingli/datasets/citygs"
 BASE_PATH_NAME="small"
 PATH_SUFFIX=""
@@ -15,7 +15,7 @@ else
     echo "--- will use standard path ---"
 fi
 SOURCE_PATH="${BASE_DIR}/data/may/small/colmap_output"
-MODEL_PATH="${BASE_DIR}/models/${BASE_PATH_NAME}${PATH_SUFFIX}_sky"
+MODEL_PATH="${BASE_DIR}/models/${BASE_PATH_NAME}${PATH_SUFFIX}_${DATE}sky"
 
 model_name=$(basename "$0" .sh)
 export CUDA_LAUNCH_BLOCKING=1
@@ -30,7 +30,7 @@ export TORCH_EXTENSIONS_ROOT=/lustre/fs12/portfolios/nvr/projects/nvr_av_foundat
 mkdir -p "$TORCH_EXTENSIONS_ROOT"
 
 PROJECT_NAME=citygs_newdata
-EXPERIENT_NAME="small_scene_sky"
+EXPERIENT_NAME="1217_2203sky"
 max_steps=150_000
 MEANS_LR=2e-3
 MEAN_LR_FINAL_MULT=1e-3
@@ -39,30 +39,30 @@ depth_lambda=2e-3
 pose_opt_start=1e5
 export PYTHONWARNINGS="ignore:The pynvml package is deprecated"
 
-# torchrun --standalone \
-#      --nproc_per_node=8 \
-#      --nnodes=1 \
-#      examples/simple_trainer_sky.py mcmc  \
-#      --data_factor 1 --data_dir $SOURCE_PATH --result_dir $MODEL_PATH \
-#      --resume \
-#      --resume_dir $MODEL_PATH \
-#      --wandb_project=$PROJECT_NAME \
-#      --wandb_group=gsplat \
-#      --wandb_name=$EXPERIENT_NAME \
-#      --wandb_mode='disabled' \
-#      --wandb_dir=$WANDB_DIR \
-#      --wandb_log_images_every=50000 \
-#      --means_lr $MEANS_LR \
-#      --mean_lr_final_mult $MEAN_LR_FINAL_MULT \
-#      --max_steps $max_steps \
-#      --depth_loss \
-#      --depth_lambda $depth_lambda \
-#      --strategy.cap-max 3000000 \
-#      --strategy.refine-start-iter 9000 \
-#      --strategy.refine-stop-iter 50000 \
-#      --strategy.refine-every 100 \
-#      --strategy.schedule-mode='original' \
-#      --strategy.densify_portion $densify_portion \
+torchrun --standalone \
+     --nproc_per_node=8 \
+     --nnodes=1 \
+     examples/simple_trainer_sky.py mcmc  \
+     --data_factor 1 --data_dir $SOURCE_PATH --result_dir $MODEL_PATH \
+     --resume \
+     --resume_dir $MODEL_PATH \
+     --wandb_project=$PROJECT_NAME \
+     --wandb_group=gsplat \
+     --wandb_name=$EXPERIENT_NAME \
+     --wandb_mode='online' \
+     --wandb_dir=$WANDB_DIR \
+     --wandb_log_images_every=50000 \
+     --means_lr $MEANS_LR \
+     --mean_lr_final_mult $MEAN_LR_FINAL_MULT \
+     --max_steps $max_steps \
+     --depth_loss \
+     --depth_lambda $depth_lambda \
+     --strategy.cap-max 3000000 \
+     --strategy.refine-start-iter 9000 \
+     --strategy.refine-stop-iter 50000 \
+     --strategy.refine-every 100 \
+     --strategy.schedule-mode='original' \
+     --strategy.densify_portion $densify_portion \
 
 echo "Training finished. Starting rendering ..."
 # python examples/render_from_ply.py \
@@ -74,8 +74,8 @@ echo "Training finished. Starting rendering ..."
 
 python examples/render_from_ckpt_sky.py \
     --cfg.data-dir $SOURCE_PATH \
-    --cfg.ply-path $MODEL_PATH/ply/point_cloud_119999.ply \
-    --cfg.ckpt $MODEL_PATH/ckpts/ckpt_119999_rank0.pt \
+    --cfg.ply-path $MODEL_PATH/ply/point_cloud_149999.ply \
+    --cfg.ckpt $MODEL_PATH/ckpts/ckpt_149999_rank0.pt \
     --cfg.cfg  $MODEL_PATH/cfg.yml \
     --cfg.result-dir $MODEL_PATH \
     --cfg.fps 15 \
