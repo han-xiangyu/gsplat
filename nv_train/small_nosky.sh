138d526dd06c240551b79a3561bb845d7e55bf5f
KarlCbw <changkarl4@gmail.com>
2026-01-13 19:24:06 +0800

    add

--- nv_train/small_nosky.sh
+++ nv_train/small_nosky.sh
@@ -55,7 +55,7 @@ nvcc --version
 torchrun --standalone \
      --nproc_per_node=2 \
      --nnodes=1 \
-     examples/simple_trainer_sky.py mcmc  \
+     examples/simple_trainer_integrated.py mcmc  \
      --data_factor 1 --data_dir $SOURCE_PATH --result_dir $MODEL_PATH \
      --resume \
      --resume_dir $MODEL_PATH \
