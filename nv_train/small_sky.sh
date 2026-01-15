56a7094bc47dda983a379015abbef438b1b22592
KarlCbw <changkarl4@gmail.com>
2026-01-16 00:04:35 +0800

    add

--- nv_train/small_sky.sh
+++ nv_train/small_sky.sh
@@ -2,7 +2,7 @@
 source /lustre/fsw/portfolios/nvr/users/ymingli/miniconda3/bin/activate
 conda activate mars_new
 cd /lustre/fsw/portfolios/nvr/users/ymingli/projects/gsplat-city/submodules/gsplat
-DATE="115_20"
+DATE="116_00"
 BASE_DIR="/lustre/fsw/portfolios/nvr/users/ymingli/datasets/citygs"
 BASE_PATH_NAME="small"
 PATH_SUFFIX=""
