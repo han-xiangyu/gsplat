5638f9ccbc5468f3203f981c97b786d9888a0c3d
KarlCbw <changkarl4@gmail.com>
2026-01-19 15:13:38 +0800

    add

--- nv_train/debug.sh
+++ nv_train/debug.sh
@@ -51,7 +51,7 @@ export PYTHONWARNINGS="ignore:The pynvml package is deprecated"
 torchrun --standalone \
      --nproc_per_node=8 \
      --nnodes=1 \
-     examples/simple_trainer_sky.py mcmc  \
+     examples/simple_trainer_integrated.py mcmc  \
      --data_factor 1 --data_dir $SOURCE_PATH --result_dir $MODEL_PATH \
      --resume \
      --resume_dir $MODEL_PATH \
