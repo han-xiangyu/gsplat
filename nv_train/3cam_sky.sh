08c47a7057795012710794c48abc3de78e44f03d
KarlCbw <changkarl4@gmail.com>
2026-01-25 21:40:01 +0800

    add

--- nv_train/3cam_sky.sh
+++ nv_train/3cam_sky.sh
@@ -2,7 +2,7 @@
 source /lustre/fsw/portfolios/nvr/users/ymingli/miniconda3/bin/activate
 conda activate mars_new
 cd /lustre/fsw/portfolios/nvr/users/ymingli/projects/gsplat-city/submodules/gsplat
-DATE="125_3sky"
+DATE="12521_3sky"
 BASE_DIR="/lustre/fsw/portfolios/nvr/users/ymingli/datasets/citygs"
 BASE_PATH_NAME="small"
 CAM="front"
