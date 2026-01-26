bf48da7677692b6674eee1afb26a1511b27907a6
KarlCbw <changkarl4@gmail.com>
2026-01-26 10:42:51 +0800

    add

--- sbatch_nv/3camsky.sh
+++ sbatch_nv/3camsky.sh
@@ -3,7 +3,7 @@
 #MODE="difix"
 MODE=""
 
-JOB_BASE_NAME="125exp3camskyposeopt"
+JOB_BASE_NAME="126exp3camskyposeopt"
 LOG_BASE_PREFIX="/lustre/fsw/portfolios/nvr/users/ymingli/datasets/citygs/log"
 
 JOB_SUFFIX=""
@@ -22,7 +22,7 @@ echo "Job Name: $job_name"
 echo "Log Dir:  $base_logdir"
 echo "Mode Arg: [${MODE:-standard}]"
 
-for i in {1..3}; do
+for i in {4..5}; do
     submit_job --gpu 8 --cpu 16 --nodes 1 \
         --partition=grizzly,polar,polar3,polar4 \
         --account=nvr_av_foundations \
