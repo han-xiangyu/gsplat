e456a758e25c97380b6051a91a2800f4fba1bde5
KarlCbw <changkarl4@gmail.com>
2026-02-03 14:13:41 +0800

    add

--- nv_train/NV_augment_difix3d.sh
+++ nv_train/NV_augment_difix3d.sh
@@ -13,6 +13,8 @@ unset LD_PRELOAD
 export TORCH_LIB=$CONDA_PREFIX/lib/python3.10/site-packages/torch/lib
 export LD_LIBRARY_PATH=$TORCH_LIB:/usr/local/cuda-11.8/lib64
 
+export PYTHONPATH=/lustre/fsw/portfolios/nvr/users/ymingli/projects/citygs/code:$PYTHONPATH
+
 # 可选护栏
 python -c "import torch; print('Using torch:', torch.__file__)"
 
