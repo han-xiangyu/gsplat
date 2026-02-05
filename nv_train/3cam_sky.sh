09da9c0a2c0c102710e5b2193c94d2941defb1ba
KarlCbw <changkarl4@gmail.com>
2026-02-05 16:51:43 +0800

    add

--- nv_train/3cam_sky.sh
+++ nv_train/3cam_sky.sh
@@ -64,7 +64,7 @@ torchrun --standalone \
      --max_steps $max_steps \
      --depth_loss \
      --depth_lambda $depth_lambda \
-     --strategy.cap-max 9000000 \
+     --strategy.cap-max 4000000 \
      --strategy.refine-start-iter 9000 \
      --strategy.refine-stop-iter 50000 \
      --strategy.refine-every 100 \
