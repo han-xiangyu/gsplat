e4df05d3759fde11ad80ff3f59bcfb28a446160f
KarlCbw <changkarl4@gmail.com>
2026-02-10 19:33:53 +0800

    add

--- sbatch_nv/3cam.sh
+++ sbatch_nv/3cam.sh
@@ -1,40 +1,27 @@
 #!/bin/bash
 
-#MODE="difix"
-MODE=""
-
-JOB_BASE_NAME="210_small_front_cam_difix"
+JOB_BASE_NAME="arlington_10k_front"
 LOG_BASE_PREFIX="/lustre/fsw/portfolios/nvr/users/ymingli/datasets/citygs/log"
-
-JOB_SUFFIX=""
-if [ "$MODE" == "difix" ]; then
-    echo "--- 'difix' is on ---"
-    JOB_SUFFIX="_difix"
-else
-    echo "--- 'standard' is on ---"
-fi
-
-job_name="${JOB_BASE_NAME}${JOB_SUFFIX}"
+job_name="${JOB_BASE_NAME}"
 base_logdir="${LOG_BASE_PREFIX}/${job_name}"
 
 echo "--- submit job ---"
 echo "Job Name: $job_name"
 echo "Log Dir:  $base_logdir"
-echo "Mode Arg: [${MODE:-standard}]"
 
-for i in {1..3}; do
-    submit_job --gpu 8 --cpu 16 --nodes 1 \
-        --partition=grizzly,polar,polar3,polar4 \
-        --account=nvr_av_foundations \
-        --image=/lustre/fsw/portfolios/nvr/users/ymingli/dockers/2304py3.sqsh \
-        --mounts=/lustre/:/lustre/,/lustre/fsw/portfolios/nvr/users/ymingli/miniconda3:/home/ymingli/miniconda3 \
-        --duration 4 \
-        --dependency=singleton \
-        --name ${job_name} \
-        --logdir ${base_logdir}/run_${i} \
-        --notimestamp \
-        --exclusive \
-        --command "bash /lustre/fsw/portfolios/nvr/users/ymingli/projects/citygs/code/gsplat/nv_train/3cam_sky.sh $MODE"
-done
+# for i in {1..2}; do
+submit_job --gpu 8 --cpu 16 --nodes 1 \
+    --partition=grizzly,polar,polar3,polar4 \
+    --account=nvr_av_foundations \
+    --image=/lustre/fsw/portfolios/nvr/users/ymingli/dockers/2304py3.sqsh \
+    --mounts=/lustre/:/lustre/,/lustre/fsw/portfolios/nvr/users/ymingli/miniconda3:/home/ymingli/miniconda3 \
+    --duration 4 \
+    --dependency=singleton \
+    --name ${job_name} \
+    --logdir ${base_logdir}/run_${i} \
+    --notimestamp \
+    --exclusive \
+    --command "bash /lustre/fsw/portfolios/nvr/users/ymingli/projects/citygs/code/gsplat/nv_train/large_front.sh"
+# done
 
 echo "--- Tasks have been submited! ---"
