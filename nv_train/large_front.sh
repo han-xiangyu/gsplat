3cbf6e3d2d7ddd2216a94846c922a09f2239538b
KarlCbw <changkarl4@gmail.com>
2026-02-10 21:12:01 +0800

    add

--- nv_train/large_front.sh
+++ nv_train/large_front.sh
@@ -63,7 +63,7 @@ depth_lambda=2e-3
 pose_opt_start=1e5
 
 torchrun --standalone \
-     --nproc_per_node=8 \
+     --nproc_per_node=1 \
      --nnodes=1 \
      examples/simple_trainer.py mcmc \
      --data_factor 1 --data_dir $SOURCE_PATH --result_dir $MODEL_PATH \
