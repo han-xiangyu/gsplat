6539e6adcb004ec85f36055ab13902aefcda1fec
KarlCbw <changkarl4@gmail.com>
2026-02-12 00:13:55 +0800

    add

--- sbatch_nv/3cam_small.sh
+++ sbatch_nv/3cam_small.sh
@@ -9,19 +9,19 @@ echo "--- submit job ---"
 echo "Job Name: $job_name"
 echo "Log Dir:  $base_logdir"
 
-# for i in {1..2}; do
-submit_job --gpu 8 --cpu 16 --nodes 1 \
-    --partition=grizzly,polar,polar3,polar4 \
-    --account=nvr_av_foundations \
-    --image=/lustre/fsw/portfolios/nvr/users/ymingli/dockers/2304py3.sqsh \
-    --mounts=/lustre/:/lustre/,/lustre/fsw/portfolios/nvr/users/ymingli/miniconda3:/home/ymingli/miniconda3 \
-    --duration 4 \
-    --dependency=singleton \
-    --name ${job_name} \
-    --logdir ${base_logdir} \
-    --notimestamp \
-    --exclusive \
-    --command "bash /lustre/fsw/portfolios/nvr/users/ymingli/projects/citygs/code/gsplat/nv_train/3cam_sky.sh"
-# done
+for i in {1..3}; do
+    submit_job --gpu 8 --cpu 16 --nodes 1 \
+        --partition=grizzly,polar,polar3,polar4 \
+        --account=nvr_av_foundations \
+        --image=/lustre/fsw/portfolios/nvr/users/ymingli/dockers/2304py3.sqsh \
+        --mounts=/lustre/:/lustre/,/lustre/fsw/portfolios/nvr/users/ymingli/miniconda3:/home/ymingli/miniconda3 \
+        --duration 4 \
+        --dependency=singleton \
+        --name ${job_name} \
+        --logdir ${base_logdir}/run_${i} \
+        --notimestamp \
+        --exclusive \
+        --command "bash /lustre/fsw/portfolios/nvr/users/ymingli/projects/citygs/code/gsplat/nv_train/3cam_sky.sh"
+done
 
 echo "--- Tasks have been submited! ---"
