8e563a931fbb40d0b688ff720b1fe2c46f60bb48
KarlCbw <changkarl4@gmail.com>
2026-02-15 01:23:19 +0800

    add

--- nv_train/3cam_sky_pose_opt.sh
+++ nv_train/3cam_sky_pose_opt.sh
@@ -3,27 +3,21 @@ set -e
 
 source /lustre/fsw/portfolios/nvr/users/ymingli/miniconda3/bin/activate
 conda activate mars_new
-
 cd /lustre/fsw/portfolios/nvr/users/ymingli/projects/gsplat-city
-
 BASE_DIR="/lustre/fsw/portfolios/nvr/users/ymingli/datasets/citygs"
-ACCOUNT="foundations"
-
-SOURCE_PATH="${BASE_DIR}/data/may/arlington_small_with_newviews"
-MODEL_PATH="${BASE_DIR}/models/arlington_small_difix_opt"
+SOURCE_PATH="${BASE_DIR}/data/may/arlington_small"
+MODEL_PATH="${BASE_DIR}/models/arlington_small_215"
 
 export CUDA_HOME=/usr/local/cuda-12.1
 export CUDACXX=$CUDA_HOME/bin/nvcc
 export PATH=$CUDA_HOME/bin:$PATH
 export LD_LIBRARY_PATH=$CUDA_HOME/lib64:$LD_LIBRARY_PATH
 export TORCH_CUDA_ARCH_LIST="8.0"
-
 export CUDA_LAUNCH_BLOCKING=1
 export TORCH_USE_CUDA_DSA=1
 
 unset PYTHONPATH_CONDA
 unset PYTHONNOUSERSITE
-
 export PYTHONPATH=/lustre/fsw/portfolios/nvr/users/ymingli/projects/gsplat-city:/lustre/fsw/portfolios/nvr/users/ymingli/projects/gsplat-city/pycolmap
 export TORCH_EXTENSIONS_DIR=/tmp/${USER}/torch_extensions/${SLURM_JOB_ID}
 mkdir -p "$TORCH_EXTENSIONS_DIR"
@@ -33,20 +27,19 @@ export WANDB_DIR="${BASE_DIR}/wandb_logs/${model_name}"
 export WANDB_API_KEY=42e7b9b31273e3a7a2bc3527a0784472e70848a2
 export WANDB_INSECURE_DISABLE_SSL=true
 export WANDB_SILENT=true
-# export TORCH_EXTENSIONS_ROOT=/lustre/fs12/portfolios/nvr/projects/nvr_av_${ACCOUNT}/users/ymingli/cache/torch_extensions_${SLURM_NODEID}
-# mkdir -p "$TORCH_EXTENSIONS_ROOT"
-
-PROJECT_NAME=citygs_newdata
-EXPERIENT_NAME="arlington_small_with_newviews_212_opt"
-
 export OMP_NUM_THREADS=1
 export PYTHONWARNINGS="ignore:The pynvml package is deprecated"
 
-max_steps=250_000
+PROJECT_NAME=citygs_newdata
+EXPERIENT_NAME="arlington_small_215"
+
+max_steps=150_000
 MEANS_LR=2e-3
+noise_lr=5e2
 MEAN_LR_FINAL_MULT=1e-3
-densify_portion=0.001
-depth_lambda=2e-3
+densify_portion=0.005
+depth_mode="disparity"
+depth_lambda=1e-1
 pose_opt_start=1e5
 
 torchrun --standalone \
@@ -65,8 +58,8 @@ torchrun --standalone \
      --means_lr $MEANS_LR \
      --mean_lr_final_mult $MEAN_LR_FINAL_MULT \
      --max_steps $max_steps \
-     --depth_loss \
-     --depth_lambda $depth_lambda \
+     --depth_loss --depth_mode $depth_mode --depth_lambda $depth_lambda --depth_from_external \
+     --strategy.noise_lr $noise_lr \
      --strategy.cap-max 400000 \
      --strategy.refine-start-iter 9000 \
      --strategy.refine-stop-iter 50000 \
@@ -74,8 +67,6 @@ torchrun --standalone \
      --strategy.densify_portion $densify_portion \
      --ground_curriculum_steps 10000 \
      --use_sky \
-     --pose_opt \
-     --pose_opt_mode "rig" \
 
 echo "Training finished. Starting rendering ..."
 python examples/render_from_ply_sky.py \
