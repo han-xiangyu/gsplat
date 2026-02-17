e05a3eceb3fa7ab5dc8a6a7c330beabdb45fa269
KarlCbw <changkarl4@gmail.com>
2026-02-17 17:41:44 +0800

    add

--- nv_train/3cam_sky_pose_opt.sh
+++ nv_train/3cam_sky_pose_opt.sh
@@ -33,18 +33,18 @@ export PYTHONWARNINGS="ignore:The pynvml package is deprecated"
 PROJECT_NAME=citygs_newdata
 EXPERIENT_NAME="arlington_small_216"
 
-max_steps=150_000
-MEANS_LR=2e-3
+max_steps=300000
+means_lr=2e-3
 noise_lr=5e2
-MEAN_LR_FINAL_MULT=1e-3
+MEAN_LR_FINAL_MULT=1e-2
 densify_portion=0.005
-depth_mode="disparity"
 depth_lambda=1e-1
-pose_opt_start=1e5
+depth_mode="disparity"
 pose_opt_start=1
 pose_opt_reg=1e-5
 pose_opt_lr=1e-3
-ground_curriculum_steps=20000
+cap_max=400_000
+ground_curriculum_steps=30000
 
 torchrun --standalone \
      --nproc_per_node=8 \
@@ -52,6 +52,9 @@ torchrun --standalone \
      examples/simple_trainer.py mcmc \
      --data_factor 1 --data_dir $SOURCE_PATH --result_dir $MODEL_PATH \
      --resume \
+     --crop_hfov_deg 90 \
+     --crop_hfov_camera_ids 4 5 6 \
+     --crop_vfov_deg 75 \
      --resume_dir $MODEL_PATH \
      --wandb_project=$PROJECT_NAME \
      --wandb_group=gsplat \
@@ -59,28 +62,35 @@ torchrun --standalone \
      --wandb_mode='online' \
      --wandb_dir=$WANDB_DIR \
      --wandb_log_images_every=50000 \
-     --means_lr $MEANS_LR \
+     --means_lr $means_lr \
      --mean_lr_final_mult $MEAN_LR_FINAL_MULT \
      --max_steps $max_steps \
      --depth_loss --depth_mode $depth_mode --depth_lambda $depth_lambda --depth_from_external \
      --strategy.noise_lr $noise_lr \
-     --strategy.cap-max 400000 \
-     --strategy.refine-start-iter 9000 \
-     --strategy.refine-stop-iter 50000 \
+     --strategy.cap-max $cap_max \
+     --strategy.refine-start-iter 30000 \
+     --strategy.refine-stop-iter 60000 \
      --strategy.refine-every 100 \
      --strategy.densify_portion $densify_portion \
      --sh_degree 1 \
+     --eval_steps 5000 10000 20000 30000 40000 50000 60000 70000 80000 90000 100000 110000 120000 130000 140000 150000 160000 170000 180000 190000 200000 225000 250000 275000 $max_steps \
+     --save_steps $max_steps \
+     --ply_steps  $max_steps \
+     --dynamic_mask \
      --ground_curriculum_steps $ground_curriculum_steps \
      --use_sky \
-     --dynamic_mask \
-     --pose_opt --pose_opt_lr $pose_opt_lr  --pose_opt_start $pose_opt_start --pose_opt_mode 'rig' --pose_opt_reg $pose_opt_reg \
+     --ground_distort_3d_lambda 5.0 \
 
 echo "Training finished. Starting rendering ..."
-python examples/render_from_ply_sky.py \
-    --data-dir $SOURCE_PATH \
-    --ply-path $MODEL_PATH/ply/point_cloud_149999.ply \
-    --ckpt-path $MODEL_PATH/ckpts/ckpt_149999_rank0.pt \
-    --use-sky \
-    --result-dir $MODEL_PATH \
+python examples/render.py \
+    --data_dir $SOURCE_PATH \
+    --crop_hfov_deg 90 \
+    --crop_hfov_camera_ids 4 5 6 \
+    --crop_vfov_deg 75 \
+    --ply_path $MODEL_PATH/ply/point_cloud_149999.ply \
+    --channels 2 1 3 5 4 6\
+    --result_dir $MODEL_PATH/video \
     --fps 15 \
-    --channels 2 1 3 \
+    --ckpt_path $MODEL_PATH/ckpts/ckpt_149999_rank0.pt \
+    --output_name ${model_name}ckpt29999_final.mp4 \
+    --use_sky\
